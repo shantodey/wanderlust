e8e54ad55d11aa4c472f598c53a15e5a8c8f28cc
shantodey <ratul3330@gmail.com>
2026-05-16 11:21:28 +0600

    connect mongoatlus to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,41 @@
+// setting dns for not getting blocked by database
+const dns=require("node:dns");
+dns.setServers(["8.8.8.8","8.8.4.4"]);
+
+
 const express = require('express')
-const app = express()
-const PORT = 5000
+const dotenv=require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+dotenv.config()
 
+const uri = process.env.MONGODB_URI;
 
+const app = express();
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+     await client.close();
+  }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+  res.send(`Server is Running on port ${PORT}`)
+})
 
 app.listen(PORT, () => {
   console.log(`Server is Running on port ${PORT}`)
