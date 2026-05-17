41f1a61ba555950ad5ad977a433749c006d5885e
shantodey <ratul3330@gmail.com>
2026-05-18 02:49:33 +0600

    using proxy to Secure front end so that without login you cannot access some pages

---
+++ client/src/proxy.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+
+
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/my-bookings', '/add-destination',['/destination/:path']], 
+}
